6314dcb0a358ead080fbb5399a52523f478450a4
naveen9036 <naveen903612@gmail.com>
2020-09-07 19:09:46 +0530

    welcome message

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Welcome, %s!";
+	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
